8a189411765b56668d303129faac7188935b428f
xpytrywork <37945582+xpytrywork@users.noreply.github.com>
2018-08-14 22:03:16 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -7,6 +7,11 @@ else
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
+  echo "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  echo "h1{color: red;}" > css/style.css
+  echo "var string = \"Hello World\"\nalert(string)" > js/main.js
+  echo 'success'
+  exit
   echo 'success'
   exit
 fi
