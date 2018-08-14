643b080ebcce9a7fbc9516d34e22ca519da0b395
xpy <840772940@qq.com>
2018-08-14 22:00:25 +0800

    demo.sh

---
+++ demo.sh
@@ -0,0 +1,12 @@
+export PATH="/c/Users/samsung/Desktop/local:$PATH"
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.html css/style.css js/main.js
+  echo 'success'
+  exit
+fi
